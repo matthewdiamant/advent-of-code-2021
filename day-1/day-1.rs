b8ad066bac0edff3510dc3467919e0f557b5402f
Matt Diamant <matthewdiamant@gmail.com>
2021-12-01 00:14:01 -0600

    day 1, rust

--- day-1/day-1.rs
+++ day-1/day-1.rs
@@ -1,9 +1,26 @@
-fn solve_part_1(_input: &Vec<char>) -> i64 {
-    return 0;
+fn solve_part_1(input: &Vec<i64>) -> i64 {
+    let mut count = 0;
+    for idx in 1..input.len() {
+        if input[idx] > input[idx - 1] {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+fn solve_part_2(input: &Vec<i64>) -> i64 {
+    let mut count = 0;
+    for idx in 3..input.len() {
+        if input[idx] > input[idx - 3] {
+            count += 1;
+        }
+    }
+    return count;
 }
 
 static INPUT: &str = include_str!("input.txt");
 fn main() {
-    let input: Vec<char> = INPUT.lines().map(|x| x.parse().unwrap()).collect();
+    let input: Vec<i64> = INPUT.lines().map(|x| x.parse().unwrap()).collect();
     println!("{}", solve_part_1(&input));
+    println!("{}", solve_part_2(&input));
 }
