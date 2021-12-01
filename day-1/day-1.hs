0fad8ff9d847d58529e49c395e4526dada9a707c
Matt Diamant <matthewdiamant@gmail.com>
2021-12-01 10:46:47 -0600

    day 1, part 1, haskell

--- day-1/day-1.hs
+++ day-1/day-1.hs
@@ -1,7 +1,11 @@
-import System.IO
-
 main = do
   contents <- readFile "input.txt"
-  putStr $ solve_part_1 contents
+  let input = map read (lines contents)
+  print (solve_part_1 input 0)
 
-solve_part_1 input = input
+solve_part_1 :: [Int] -> Int -> Int
+solve_part_1 [x] count = count
+solve_part_1 (x:xs) count =
+  solve_part_1 xs (count + countIncrease x (head xs))
+
+countIncrease a b = if b > a then 1 else 0
