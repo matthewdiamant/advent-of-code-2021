14412b74a799c434931c9363671f05f5c324b963
Matt Diamant <matthewdiamant@gmail.com>
2021-12-01 11:01:27 -0600

    day 1, cleanup, haskell

--- day-1/day-1.hs
+++ day-1/day-1.hs
@@ -1,14 +1,18 @@
 main = do
   contents <- readFile "input.txt"
-  let input = map read (lines contents)
-  print (solve input 0 0)
-  print (solve input 2 0)
+  print (solve (depths contents) 0 0)
+  print (solve (depths contents) 2 0)
+
+depths :: [Char] -> [Int]
+depths contents = map read (lines contents)
 
 solve :: [Int] -> Int -> Int -> Int
 solve (x:xs) idx count =
   if length xs == idx
   then count
   else
-    solve xs idx (count + countIncrease x (xs !! idx))
+    solve xs idx (count + countIncrease (x < (xs !! idx)))
 
-countIncrease a b = if b > a then 1 else 0
+countIncrease :: Bool -> Int
+countIncrease True = 1
+countIncrease False = 0
