aa2bd15ae98b1fab251c05f5c2e9e183507b53f2
Matt Diamant <matthewdiamant@gmail.com>
2021-12-01 21:42:03 -0800

    day 2, clean up, rust

--- day-2/day-2.rs
+++ day-2/day-2.rs
@@ -1,10 +1,15 @@
+fn parse_instruction(input: &str) -> (&str, i64) {
+    let instruction = input.split(' ').collect::<Vec<&str>>();
+    let direction = instruction[0];
+    let value: i64 = instruction[1].parse().unwrap();
+    return (direction, value);
+}
+
 fn solve_part_1(inputs: &Vec<String>) -> i64 {
     let mut horizontal = 0;
     let mut depth = 0;
     for input in inputs {
-        let instruction = input.split(' ').collect::<Vec<&str>>();
-        let direction = instruction[0];
-        let value: i64 = instruction[1].parse().unwrap();
+        let (direction, value) = parse_instruction(input);
         if direction == "up" {
             depth -= value;
         }
@@ -23,9 +28,7 @@ fn solve_part_2(inputs: &Vec<String>) -> i64 {
     let mut depth = 0;
     let mut aim = 0;
     for input in inputs {
-        let instruction = input.split(' ').collect::<Vec<&str>>();
-        let direction = instruction[0];
-        let value: i64 = instruction[1].parse().unwrap();
+        let (direction, value) = parse_instruction(input);
         if direction == "up" {
             aim -= value;
         }
