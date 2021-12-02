33fc345c958dfdd76b318510290a8503beb3a9e7
Matt Diamant <matthewdiamant@gmail.com>
2021-12-01 22:40:09 -0800

    day 2, part 2, haskell

--- day-2/day-2.hs
+++ day-2/day-2.hs
@@ -1,8 +1,10 @@
 main = do
   contents <- readFile "input.txt"
-  print (solve_part_1 contents)
+  print (solvePart1 contents)
+  print (solvePart2 contents)
 
-solve_part_1 input = drive (parseInput input) 0 0
+solvePart1 :: String -> Int
+solvePart1 input = drive1 (parseInput input) 0 0
 
 parseInput :: String -> [(String, Int)]
 parseInput input = map createInstruction (lines input)
@@ -10,9 +12,9 @@ parseInput input = map createInstruction (lines input)
 createInstruction :: String -> (String, Int)
 createInstruction line = (head (words line), read (words line !! 1) :: Int)
 
-drive :: [(String, Int)] -> Int -> Int -> Int
-drive [] horizontal depth = horizontal * depth
-drive (x:xs) horizontal depth = drive xs (changeHorizontal horizontal x) (changeDepth depth x)
+drive1 :: [(String, Int)] -> Int -> Int -> Int
+drive1 [] horizontal depth = horizontal * depth
+drive1 (x:xs) horizontal depth = drive1 xs (changeHorizontal horizontal x) (changeDepth depth x)
 
 changeHorizontal :: Int -> (String, Int) -> Int
 changeHorizontal horizontal (direction, value) =
@@ -25,3 +27,22 @@ changeDepth depth (direction, value)
   | direction == "up" = depth - value
   | direction == "down" = depth + value
   | otherwise = depth
+
+solvePart2 :: String -> Int
+solvePart2 input = drive2 (parseInput input) (0, 0) 0
+
+drive2 :: [(String, Int)] -> (Int, Int) -> Int -> Int
+drive2 [] (horizontal, depth) aim = horizontal * depth
+drive2 (x:xs) (horizontal, depth) aim = drive2 xs (goForward (horizontal, depth) aim x) (changeAim aim x)
+
+goForward :: (Int, Int) -> Int -> (String, Int) -> (Int, Int)
+goForward (horizontal, depth) aim (direction, value) =
+    if direction == "forward"
+       then (horizontal + value, depth + aim * value)
+       else (horizontal, depth)
+
+changeAim :: Int -> (String, Int) -> Int
+changeAim aim (direction, value)
+  | direction == "up" = aim - value
+  | direction == "down" = aim + value
+  | otherwise = aim
