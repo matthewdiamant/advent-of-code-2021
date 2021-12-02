19514690a94ea59cf821f9c3722e4015f7e605c7
Matt Diamant <matthewdiamant@gmail.com>
2021-12-01 22:18:22 -0800

    day 2, part 1, haskell

--- day-2/day-2.hs
+++ day-2/day-2.hs
@@ -1,7 +1,27 @@
-import System.IO
-
 main = do
   contents <- readFile "input.txt"
-  putStr $ solve_part_1 contents
+  print (solve_part_1 contents)
 
-solve_part_1 input = input
+solve_part_1 input = drive (parseInput input) 0 0
+
+parseInput :: String -> [(String, Int)]
+parseInput input = map createInstruction (lines input)
+
+createInstruction :: String -> (String, Int)
+createInstruction line = (head (words line), read (words line !! 1) :: Int)
+
+drive :: [(String, Int)] -> Int -> Int -> Int
+drive [] horizontal depth = horizontal * depth
+drive (x:xs) horizontal depth = drive xs (changeHorizontal horizontal x) (changeDepth depth x)
+
+changeHorizontal :: Int -> (String, Int) -> Int
+changeHorizontal horizontal (direction, value) =
+    if direction == "forward"
+       then horizontal + value
+       else horizontal
+
+changeDepth :: Int -> (String, Int) -> Int
+changeDepth depth (direction, value)
+  | direction == "up" = depth - value
+  | direction == "down" = depth + value
+  | otherwise = depth
